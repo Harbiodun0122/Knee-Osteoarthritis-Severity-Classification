85a7d6d7e7492aa3dd8f32af9e0628856bf3a602
Abiodun <67191471+Harbiodun0122@users.noreply.github.com>
2025-05-20 05:14:42 +0100

    Delete postinstall.sh

--- postinstall.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-# Optional: Print environment info
-echo "Python version: $(python --version)"
-echo "Pip version: $(pip --version)"
-
-# Install detectron2 manually
-pip install git+https://github.com/facebookresearch/detectron2.git@v0.6
